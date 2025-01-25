52735d11bbfa4a75dac50be32fdaf1d4d76809ec
Sandeep Reddy <sandeepreddybalireddy@gmail.com>
2025-01-25 21:30:09 +0530

    Create a static task and delete task after logging 10 iterations.

---
+++ first_project/user_app/include/task3.h
@@ -0,0 +1,16 @@
+/*
+ * task3.h
+ *
+ *  Created on: Jan 25, 2025
+ *      Author: sandeep
+ */
+
+#ifndef INCLUDE_TASK3_H_
+#define INCLUDE_TASK3_H_
+
+/* stack size in words not bytes so it is actually 200*4 bytes */
+#define TASK3_STACK_SIZE 200
+#define TASK3_PRIORITY   2
+void task3_init(void);
+
+#endif /* INCLUDE_TASK3_H_ */
--- first_project/user_app/user_app_main.c
+++ first_project/user_app/user_app_main.c
@@ -5,12 +5,13 @@
  *      Author: sandeep
  */
 
+#include "FreeRTOS.h"
+#include "task.h"
 
 #include "user_app_main.h"
 #include "task1.h"
 #include "task2.h"
-#include "FreeRTOS.h"
-#include "task.h"
+#include "task3.h"
 
 void user_app_init(void)
 {
@@ -18,6 +19,8 @@ void user_app_init(void)
 	task1_init();
 
 	task2_init();
+
+	task3_init();
 
     // Start the real time scheduler.
     vTaskStartScheduler();
